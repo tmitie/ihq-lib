6dadb59946bb9120a34f1cb704ef5fbc24a23993
Thaynara Tessaline Mitie Sei Soares <thaynara.mitie@outlook.com>
2025-06-12 22:10:06 -0300

    ajuste de caminho da imagem amostra

--- main_from_lib.py
+++ main_from_lib.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from process_img.process_img_interface import process_image_with_boundaries
 
 if __name__ == "__main__":
-    image_path = "amostra_exemplo.JPG"
+    image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "amostra_exemplo.JPG")
 
     limiteInferiorPositivo = (34, 20, 34)
     limiteSuperiorPositivo = None
